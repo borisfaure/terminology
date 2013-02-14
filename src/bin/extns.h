af064b227a8bb761cd621bb53e56e1aca966f49e
Carsten Haitzler <raster@rasterman.com>
2013-02-14 15:53:55 +0000

    extn info repeated and.. missing.
    SVN revision: 83897

--- src/bin/extns.h
+++ src/bin/extns.h
@@ -1,10 +1,10 @@
 static const char *extn_img[] =
 {
-   ".png", ".jpg", ".jpeg", ".jpe", ".jfif", ".tif", ".tiff", ".gif",
+   ".png", ".jpg", ".jpeg", ".jpe", ".jfif", ".tif", ".tiff", ".gif", ".thm",
    ".bmp", ".ico", ".ppm", ".pgm", ".pbm", ".pnm", ".xpm", ".psd", ".wbmp",
    ".cur", ".xcf", ".xcf.gz", ".arw", ".cr2", ".crw", ".dcr", ".dng", ".k25",
    ".kdc", ".erf", ".mrw", ".nef", ".nrf", ".nrw", ".orf", ".raw", ".rw2",
-   ".rw2", ".pef", ".raf", ".sr2", ".srf", ".x3f", ".webp", 
+   ".pef", ".raf", ".sr2", ".srf", ".x3f", ".webp", 
    NULL
 };
 
