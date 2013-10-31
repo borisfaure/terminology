borisfaure/terminology

commit 82f4d3546a78a8f0280ac912816e86f6b2c77404

improve previous smart queue commit by deleting existing animator
this completely fixes annoying issues where mousing out of the terminal fails to de-linkify text

moderately reviewed by: billiob

src/bin/termio.c
@@ -4485,7 +4485,12 @@ termio_mouseover_suspend_pushpop(Evas_Object *obj, int dir)
    if (!sd) return;
    sd->link.suspend += dir;
    if (sd->link.suspend < 0) sd->link.suspend = 0;
-   if (!sd->link.suspend)
+   if (sd->link.suspend)
+     {
+        if (sd->anim) ecore_animator_del(sd->anim);
+        sd->anim = NULL;
+     }
+   else
      _smart_update_queue(obj, sd);
 }
 

commit 5958eff3380552febfe4dba397ee04dae9ae31d8

unbreak inlined media being unparented when tab selector is used
82f4d3546a78a8f0280ac912816e86f6b2c77404 introduced this bug (oct 20
2013 - discomfitor). now missing a _smart_apply() causes the bug. why
- not sure.

src/bin/termio.c
@@ -4489,6 +4489,8 @@ termio_mouseover_suspend_pushpop(Evas_Object *obj, int dir)
      {
         if (sd->anim) ecore_animator_del(sd->anim);
         sd->anim = NULL;
+        _smart_apply(obj);
+//        evas_object_smart_callback_call(obj, "changed", NULL);
      }
    else
      _smart_update_queue(obj, sd);