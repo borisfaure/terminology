c59bce60f7ee21f6cff0a34f3ef67e9f286abf19
Boris Faure <billiob@gmail.com>
2014-03-17 22:32:25 +0100

    col.h needs config.h

--- src/bin/col.h
+++ src/bin/col.h
@@ -2,6 +2,7 @@
 #define _COL_H__ 1
 
 #include <Evas.h>
+#include "config.h"
 
 void colors_term_init(Evas_Object *textgrid, Evas_Object *bg, Config *config);
 void colors_standard_get(int set, int col, unsigned char *r, unsigned char *g, unsigned char *b, unsigned char *a);
