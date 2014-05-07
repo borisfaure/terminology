6648388c8ff913ebdab09c110f882973faf8dcb8
Boris Faure <billiob@gmail.com>
2014-05-07 21:02:19 +0200

    add pacman.sh cause it's cool!

---
+++ tools/pacman.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# ANSI Color -- use these variables to easily have different color
+#    and format output. Make sure to output the reset sequence after 
+#    colors (f = foreground, b = background), and use the 'off'
+#    feature for anything you turn on.
+
+initializeANSI()
+{
+    esc=$(echo -en "\033")
+  blackf="${esc}[30m";   redf="${esc}[31m";    greenf="${esc}[32m"
+  yellowf="${esc}[33m"   bluef="${esc}[34m";   purplef="${esc}[35m"
+  cyanf="${esc}[36m";    whitef="${esc}[37m"
+  
+  blackb="${esc}[40m";   redb="${esc}[41m";    greenb="${esc}[42m"
+  yellowb="${esc}[43m"   blueb="${esc}[44m";   purpleb="${esc}[45m"
+  cyanb="${esc}[46m";    whiteb="${esc}[47m"
+
+  boldon="${esc}[1m";    boldoff="${esc}[22m"
+  italicson="${esc}[3m"; italicsoff="${esc}[23m"
+  ulon="${esc}[4m";      uloff="${esc}[24m"
+  invon="${esc}[7m";     invoff="${esc}[27m"
+
+  reset="${esc}[0m"
+}
+
+# note in this first use that switching colors doesn't require a reset
+# first - the new color overrides the old one.
+
+#clear 
+
+initializeANSI
+
+cat << EOF
+
+ ${yellowf}  .#######.${reset}   ${redf}  .######.${reset}    ${greenf}  .######.${reset}    ${bluef}  .######.${reset}    ${purplef}  .######.${reset}    ${cyanf}  .######.${reset}
+ ${yellowf}.#########**${reset}  ${redf}.${whitef}#*#${redf}##${whitef}#*#${redf}##.${reset}  ${greenf}.${whitef}#*#${greenf}##${whitef}#*#${greenf}##.${reset}  ${bluef}.${whitef}#*#${bluef}##${whitef}#*#${bluef}##.${reset}  ${purplef}.${whitef}#*#${purplef}##${whitef}#*#${purplef}##.${reset}  ${cyanf}.${whitef}#*#${cyanf}##${whitef}#*#${cyanf}##.${reset}
+ ${yellowf}#######*${reset}      ${redf}#${whitef}..#${redf}##${whitef}..#${redf}###${reset}  ${greenf}#${whitef}..#${greenf}##${whitef}..#${greenf}###${reset}  ${bluef}#${whitef}..#${bluef}##${whitef}..#${bluef}###${reset}  ${purplef}#${whitef}..#${purplef}##${whitef}..#${purplef}###${reset}  ${cyanf}#${whitef}..#${cyanf}##${whitef}..#${cyanf}###${reset}
+ ${yellowf}#######.${reset}      ${redf}############${reset}  ${greenf}############${reset}  ${bluef}############${reset}  ${purplef}############${reset}  ${cyanf}############${reset}
+ ${yellowf}*#########..${reset}  ${redf}##*##**##*##${reset}  ${greenf}##*##**##*##${reset}  ${bluef}##*##**##*##${reset}  ${purplef}##*##**##*##${reset}  ${cyanf}##*##**##*##${reset}
+ ${yellowf}  *#######*${reset}   ${redf}*   *  *   *${reset}  ${greenf}*   *  *   *${reset}  ${bluef}*   *  *   *${reset}  ${purplef}*   *  *   *${reset}  ${cyanf}*   *  *   *${reset}
+ 
+ ${boldon}${yellowf}  .#######.   ${redf}  .######.    ${greenf}  .######.    ${bluef}  .######.    ${purplef}  .######.    ${cyanf}  .######.${reset}
+ ${boldon}${yellowf}.#########**  ${redf}.${whitef}#*#${redf}##${whitef}#*#${redf}##.  ${greenf}.${whitef}#*#${greenf}##${whitef}#*#${greenf}##.  ${bluef}.${whitef}#*#${bluef}##${whitef}#*#${bluef}##.  ${purplef}.${whitef}#*#${purplef}##${whitef}#*#${purplef}##.  ${cyanf}.${whitef}#*#${cyanf}##${whitef}#*#${cyanf}##.${reset}
+ ${boldon}${yellowf}#######*      ${redf}#${whitef}..#${redf}##${whitef}..#${redf}###  ${greenf}#${whitef}..#${greenf}##${whitef}..#${greenf}###  ${bluef}#${whitef}..#${bluef}##${whitef}..#${bluef}###  ${purplef}#${whitef}..#${purplef}##${whitef}..#${purplef}###  ${cyanf}#${whitef}..#${cyanf}##${whitef}..#${cyanf}###${reset}
+ ${boldon}${yellowf}#######.      ${redf}############  ${greenf}############  ${bluef}############  ${purplef}############  ${cyanf}############${reset}
+ ${boldon}${yellowf}*#########..  ${redf}##*##**##*##  ${greenf}##*##**##*##  ${bluef}##*##**##*##  ${purplef}##*##**##*##  ${cyanf}##*##**##*##${reset}
+ ${boldon}${yellowf}  *#######*   ${redf}*   *  *   *  ${greenf}*   *  *   *  ${bluef}*   *  *   *  ${purplef}*   *  *   *  ${cyanf}*   *  *   *${reset}
+
+EOF
