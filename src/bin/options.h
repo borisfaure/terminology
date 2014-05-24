d194d406a301fbe19c9a57431ad24e4d342662c4
Boris Faure <billiob@gmail.com>
2014-05-24 16:14:20 +0200

    add guard header

--- src/bin/options.h
+++ src/bin/options.h
@@ -1,3 +1,8 @@
+#ifndef _TERMINOLOGY_OPTIONS_H__
+#define _TERMINOLOGY_OPTIONS_H__ 1
+
 void options_toggle(Evas_Object *win, Evas_Object *bg, Evas_Object *term,
                     void (*donecb) (void *data), void *donedata);
 Eina_Bool options_active_get(void);
+
+#endif
