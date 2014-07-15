0f463fc4bf4b6cec792365cb5b51a9fa798c34f4
wonguk.jeong <wonguk.jeong@samsung.com>
2014-07-15 12:18:46 +0200

    tycat: show a media properly in precise area
    Summary:
    Precise multimedia size/type/aspect ratio could be acquired after first frame has been decoded.
    
    Therefore,
    
    Fixes T1376
    
    1. put every file in the queue, and handle them one by one
    2. in case of video, block queue handling until "frame_decode" callback is invoked.
    3. to avoid infinite blocking, put timer for timeout check
    
    Test Plan:
    1.terminology -> tycat "video file" -> check whether the video size is fit to real video size
    2.            -> tycat "audio file" -> check whether black area is shown or not
    
    Reviewers: billiob, raster
    
    Subscribers: seoz
    
    Maniphest Tasks: T1376
    
    Differential Revision: https://phab.enlightenment.org/D1192

--- src/bin/tycat.c
+++ src/bin/tycat.c
@@ -12,17 +12,18 @@
 #include <string.h>
 
 enum {
-   CENTER,
-   FILL,
-   STRETCH,
-   NOIMG
+  CENTER,
+  FILL,
+  STRETCH,
+  NOIMG
 };
 
-Ecore_Evas *ee = NULL;
-Evas *evas = NULL;
-Evas_Object *o = NULL;
-struct termios told, tnew;
-int tw = 0, th = 0, cw = 0, ch = 0, maxw = 0, maxh = 0;
+#define VIDEO_DECODE_TIMEOUT 1.0
+
+static Evas *evas = NULL;
+static struct termios told, tnew;
+static int tw = 0, th = 0, cw = 0, ch = 0, maxw = 0, maxh = 0, mode = CENTER;
+static Ecore_Timer *timeout_t = NULL;
 
 #include "extns.h"
 
@@ -67,7 +68,7 @@ static const char *
 is_fmt(const char *f, const char **extn)
 {
    int i, len, l;
-   
+
    len = strlen(f);
    for (i = 0; extn[i]; i++)
      {
@@ -126,20 +127,195 @@ print_usage(const char *argv0)
          argv0);
 }
 
+static Eina_Bool
+timeout_cb(void *data)
+{
+   evas_object_del(data);
+   timeout_t = NULL;
+
+   return ECORE_CALLBACK_CANCEL;
+}
+
+static int
+handle_image(char *rp)
+{
+   Evas_Object *o;
+   int w = 0, h = 0;
+   int iw = 0, ih = 0;
+   int r = -1;
+
+   if (!is_fmt(rp, extn_img) &&
+       !is_fmt(rp, extn_scale) &&
+       !is_fmt(rp, extn_mov))
+     return -1;
+
+   o = evas_object_image_add(evas);
+   evas_object_image_file_set(o, rp, NULL);
+   evas_object_image_size_get(o, &w, &h);
+   if ((w >= 0) && (h > 0))
+     {
+        scaleterm(w, h, &iw, &ih);
+        prnt(rp, iw, ih, mode);
+        r = 0;
+     }
+
+   evas_object_del(o);
+
+   return r;
+}
+
+static int
+handle_edje(char *rp)
+{
+   Evas_Object *o;
+   int w = 0, h = 0;
+   int iw = 0, ih = 0;
+   int r = -1;
+
+   if (!is_fmt(rp, extn_edj)) return -1;
+
+   o = edje_object_add(evas);
+   if (edje_object_file_set
+       (o, rp, "terminology/backgroud") == EINA_TRUE ||
+       !edje_object_file_set
+       (o, rp, "e/desktop/background") == EINA_TRUE)
+     {
+        Evas_Coord mw = 0, mh = 0;
+        edje_object_size_min_get(o, &mw, &mh);
+        if ((mw <= 0) || (mh <= 0))
+          edje_object_size_min_calc(o, &mw, &mh);
+        if ((mw <= 0) || (mh <= 0))
+          {
+             mw = (tw) * cw;
+             mh = (th - 1) * ch;
+          }
+        scaleterm(mw, mh, &iw, &ih);
+        prnt(rp, iw, ih, mode);
+        r = 0;
+     }
+
+   evas_object_del(o);
+
+   return r;
+}
+
+static void
+video_decoded(void *data, Evas_Object *o, void *ei EINA_UNUSED)
+{
+   int w = 0, h = 0;
+   int iw = 0, ih = 0;
+
+   if (emotion_object_video_handled_get(o))
+     {
+        emotion_object_size_get(o, &w, &h);
+        if ((w >= 0) && (h > 0))
+          {
+             scaleterm(w, h, &iw, &ih);
+             prnt(data, iw, ih, mode);
+             goto done;
+          }
+        else
+          {
+             double ar = emotion_object_ratio_get(o);
+             if (ar > 0.0)
+               {
+                  scaleterm(tw * cw, (int) ((tw * cw) / ar), &iw, &ih);
+                  prnt(data, iw, ih, mode);
+                  goto done;
+               }
+          }
+     }
+
+   prnt(data, tw, 3, NOIMG);
+
+done:
+   ecore_timer_del(timeout_t);
+   timeout_t = NULL;
+   evas_object_del(o);
+   free(data);
+}
+
+static int
+handle_video(char *rp)
+{
+   Evas_Object *o;
+
+   if (!is_fmt(rp, extn_aud) &&
+       !is_fmt(rp, extn_mov))
+     return -1;
+
+   o = emotion_object_add(evas);
+   if (emotion_object_init(o, NULL) == EINA_TRUE)
+     {
+        if (emotion_object_file_set(o, rp))
+          {
+             emotion_object_audio_mute_set(o, EINA_TRUE);
+             emotion_object_play_set(o, EINA_TRUE);
+             timeout_t = ecore_timer_add(VIDEO_DECODE_TIMEOUT,
+                                         timeout_cb, o);
+             evas_object_smart_callback_add(o, "frame_decode",
+                                            video_decoded,
+                                            strdup(rp));
+             return 0;
+          }
+
+     }
+
+   evas_object_del(o);
+
+   return -1;
+}
+
+static Eina_Bool
+handle_file(void *data)
+{
+   Eina_List **file_q = data;
+
+   int (*handlers[])(char *rp) = {
+     handle_image,
+     handle_edje,
+     handle_video,
+     NULL
+   };
+   char *rp;
+   int i;
+
+   if (timeout_t) return ECORE_CALLBACK_RENEW;
+   if (!(*file_q))
+     {
+        ecore_main_loop_quit();
+        return ECORE_CALLBACK_CANCEL;
+     }
+
+   rp = eina_list_data_get(*file_q);
+   *file_q = eina_list_remove_list(*file_q, *file_q);
+   if (!rp) return ECORE_CALLBACK_RENEW;
+
+   for (i = 0; handlers[i]; i++)
+     {
+        if (handlers[i](rp) == 0) break;
+     }
+   free(rp);
+
+   return ECORE_CALLBACK_RENEW;
+}
+
 int
 main(int argc, char **argv)
 {
+   Ecore_Evas *ee;
    char buf[64];
-   int w = 0, h = 0;
-   int iw = 0, ih = 0;
-   int ret = 0;
-   
+   int i;
+   char *rp;
+   Eina_List *file_q = NULL;
+
    if (!getenv("TERMINOLOGY")) return 0;
    if (argc <= 1)
      {
         print_usage(argv[0]);
         return 0;
      }
+
    eina_init();
    ecore_init();
    ecore_file_init();
@@ -147,178 +323,80 @@ main(int argc, char **argv)
    ecore_evas_init();
    edje_init();
    emotion_init();
+
    ee = ecore_evas_buffer_new(1, 1);
-   if (ee)
+   if (!ee) goto shutdown;
+   evas = ecore_evas_get(ee);
+   echo_off();
+   snprintf(buf, sizeof(buf), "%c}qs", 0x1b);
+   if (write(0, buf, strlen(buf) + 1) < 0) perror("write");
+   if (scanf("%i;%i;%i;%i", &tw, &th, &cw, &ch) != 4 ||
+       ((tw <= 0) || (th <= 0) || (cw <= 1) || (ch <= 1)))
      {
-        int i, mode = CENTER;
-        
-        evas = ecore_evas_get(ee);
-        echo_off();
-        snprintf(buf, sizeof(buf), "%c}qs", 0x1b);
-        if (write(0, buf, strlen(buf) + 1) < 0) perror("write");
-        if (scanf("%i;%i;%i;%i", &tw, &th, &cw, &ch) != 4)
-          {
-             echo_on();
-             return 0;
-          }
-        if ((tw <= 0) || (th <= 0) || (cw <= 1) || (ch <= 1))
-          {
-             echo_on();
-             return 0;
-          }
         echo_on();
-        for (i = 1; i < argc; i++)
-          {
-             char *path, *rp;
-
-             if (!strcmp(argv[i], "-c"))
-               {
-                  mode = CENTER;
-                  i++;
-                  if (i >= argc) return 0;
-               }
-             else if (!strcmp(argv[i], "-s"))
-               {
-                  mode = STRETCH;
-                  i++;
-                  if (i >= argc) return 0;
-               }
-             else if (!strcmp(argv[i], "-f"))
-               {
-                  mode = FILL;
-                  i++;
-                  if (i >= argc) return 0;
-               }
-
-             if (!strcmp(argv[i], "-g"))
-               {
-                  unsigned int width = 0, height = 0;
-                  int cnt;
-
-                  if (i + 2 >= argc)
-                    {
-                       print_usage(argv[0]);
-                       ret = 1;
-                       goto shutdown;
-                    }
-                  i++;
-                  cnt = sscanf(argv[i], "%ux%u", &width, &height);
-                  if (cnt != 2)
-                    {
-                       print_usage(argv[0]);
-                       ret = 1;
-                       goto shutdown;
-                    }
-                  if (!width || tw > (int)width) maxw = width;
-                  maxh = height;
-               }
-
-             path = argv[i];
-             rp = ecore_file_realpath(path);
-             if (rp)
-               {
-                  if ((is_fmt(rp, extn_img)) ||
-                      (is_fmt(rp, extn_scale)) ||
-                      (is_fmt(rp, extn_mov)))
-                    {
-                       o = evas_object_image_add(evas);
-                       evas_object_image_file_set(o, rp, NULL);
-                       evas_object_image_size_get(o, &w, &h);
-                       if ((w >= 0) && (h > 0))
-                         {
-                            scaleterm(w, h, &iw, &ih);
-                            prnt(rp, iw, ih, mode);
-                            goto done;
-                         }
-                       evas_object_del(o);
-                       o = NULL;
-                    }
-                  
-                  if (is_fmt(rp, extn_edj))
-                    {
-                       Eina_Bool ok = EINA_TRUE;
-                       
-                       evas_object_del(o);
-                       o = edje_object_add(evas);
-                       if (!edje_object_file_set
-                           (o, rp, "terminology/backgroud"))
-                         {
-                            if (!edje_object_file_set
-                                (o, rp, "e/desktop/background"))
-                              ok = EINA_FALSE;
-                         }
-                       if (ok)
-                         {
-                            Evas_Coord mw = 0, mh = 0;
-                            
-                            edje_object_size_min_get(o, &mw, &mh);
-                            if ((mw <= 0) || (mh <= 0))
-                              edje_object_size_min_calc(o, &mw, &mh);
-                            if ((mw <= 0) || (mh <= 0))
-                              {
-                                 mw = (tw) * cw;
-                                 mh = (th - 1) * ch;
-                              }
-                            scaleterm(mw, mh, &iw, &ih);
-                            prnt(rp, iw, ih, mode);
-                            goto done;
-                         }
-                       evas_object_del(o);
-                       o = NULL;
-                    }
-                  
-                  if ((is_fmt(rp, extn_aud)) ||
-                      (is_fmt(rp, extn_mov)))
-                    {
-                       Eina_Bool ok = EINA_TRUE;
-                       
-                       o = emotion_object_add(evas);
-                       ok = emotion_object_init(o, NULL);
-                       if (ok)
-                         {
-                            if (emotion_object_file_set(o, rp))
-                              {
-                                 emotion_object_audio_mute_set(o, EINA_TRUE);
-                                 if (emotion_object_video_handled_get(o))
-                                   {
-                                      emotion_object_size_get(o, &w, &h);
-                                      if ((w >= 0) && (h > 0))
-                                        {
-                                           scaleterm(w, h, &iw, &ih);
-                                           prnt(rp, iw, ih, mode);
-                                        }
-                                      else ok = EINA_FALSE;
-                                   }
-                                 else ok = EINA_FALSE;
-
-                                 if (!ok)
-                                   {
-                                      double ar = emotion_object_ratio_get(o);
-                                      if (ar > 0.0)
-                                        {
-                                           scaleterm(tw * cw, (int) ((tw * cw) / ar), &iw, &ih);
-                                           prnt(rp, iw, ih, mode);
-                                        }
-                                      else
-                                        prnt(rp, tw, 3, NOIMG);
-                                   }
-                                 goto done;
-                              }
-                         }
-                       evas_object_del(o);
-                       o = NULL;
-                    }
-done:
-                  if (o) evas_object_del(o);
-                  o = NULL;
-                  free(rp);
-               }
-             evas_norender(evas);
-          }
-        exit(0);
-//        ecore_main_loop_begin();
-        ecore_evas_free(ee);
+        goto shutdown;
      }
+   echo_on();
+
+   for (i = 1; i < argc; i++)
+     {
+        char *path;
+
+        if (!strcmp(argv[i], "-c"))
+          {
+             mode = CENTER;
+             i++;
+             if (i >= argc) goto done;
+          }
+        else if (!strcmp(argv[i], "-s"))
+          {
+             mode = STRETCH;
+             i++;
+             if (i >= argc) goto done;
+          }
+        else if (!strcmp(argv[i], "-f"))
+          {
+             mode = FILL;
+             i++;
+             if (i >= argc) goto done;
+          }
+
+        if (!strcmp(argv[i], "-g"))
+          {
+             unsigned int width = 0, height = 0;
+             int cnt;
+
+             if (i + 2 >= argc)
+               {
+                  print_usage(argv[0]);
+                  goto shutdown;
+               }
+             i++;
+             cnt = sscanf(argv[i], "%ux%u", &width, &height);
+             if (cnt != 2)
+               {
+                  print_usage(argv[0]);
+                  goto done;
+               }
+             if (!width || tw > (int)width) maxw = width;
+             maxh = height;
+          }
+        path = argv[i];
+        rp = ecore_file_realpath(path);
+        if (rp)
+          file_q = eina_list_append(file_q, rp);
+     }
+   if (!file_q) goto done;
+
+   ecore_idler_add(handle_file, &file_q);
+   ecore_main_loop_begin();
+
+done:
+   EINA_LIST_FREE(file_q, rp)
+     free(rp);
+
+   ecore_evas_free(ee);
+
 shutdown:
    emotion_shutdown();
    edje_shutdown();
@@ -327,5 +405,6 @@ shutdown:
    ecore_file_shutdown();
    ecore_shutdown();
    eina_shutdown();
-   return ret;
+
+   return 0;
 }
