93e2d73a872f4b66ac586103f6c2c83512fd65eb
godfath3r <galatsanosp@gmail.com>
2014-08-17 22:14:27 +0200

    options_keys: update shift+Up/Down for -1/+1 line scroll
    Reviewers: billiob
    
    Differential Revision: https://phab.enlightenment.org/D1339

--- src/bin/options_keys.c
+++ src/bin/options_keys.c
@@ -32,6 +32,8 @@ options_keys(Evas_Object *opbox, Evas_Object *term EINA_UNUSED)
 
    KB(_("Scroll one page up"), "Shift + PgUp");
    KB(_("Scroll one page down"), "Shift + PgDn");
+   KB(_("Scroll one line up"), "Shift + Up");
+   KB(_("Scroll one line down"), "Shift + Down");
    KB(_("Paste Clipboard (ctrl+v/c) selection"), "Shift + Insert");
    KB(_("Paste Clipboard (ctrl+v/c) selection"), "Ctrl + Shift + v");
    KB(_("Paste Primary (highlight) selection"), "Shift + Ctrl + Insert");
