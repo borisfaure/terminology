9879be313b12fe2dd18c3810464e4dd327aa1534
Chris Michael <cp.michael@samsung.com>
2014-09-19 09:31:55 -0400

    terminology: Fix compile error if gettext & nls are not enabled
    Summary: If we do not enable gettext & nls support, then LOCALEDIR is
    undefined and thus terminology would not compile. Fix that by only
    using LOCALEDIR if they are defined.
    
    Break introduced with ad86a427e7b72a41f3f0451f3597d3b57d59c5b0
    
    @fix
    
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/bin/main.c
+++ src/bin/main.c
@@ -2972,7 +2972,9 @@ elm_main(int argc, char **argv)
    elm_app_compile_bin_dir_set(PACKAGE_BIN_DIR);
    elm_app_compile_lib_dir_set(PACKAGE_LIB_DIR);
    elm_app_compile_data_dir_set(PACKAGE_DATA_DIR);
+#if HAVE_GETTEXT && ENABLE_NLS
    elm_app_compile_locale_set(LOCALEDIR);
+#endif
    elm_app_info_set(elm_main, "terminology", "themes/default.edj");
 
 #if HAVE_GETTEXT && ENABLE_NLS
