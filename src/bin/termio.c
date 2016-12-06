45539d433c5f723c11dd51f4d0d740fe24665dc3
Boris Faure <billiob@gmail.com>
2016-12-06 23:49:03 +0100

    termio: small cleanup

--- src/bin/termio.c
+++ src/bin/termio.c
@@ -5098,16 +5098,8 @@ _smart_size(Evas_Object *obj, int w, int h, Eina_Bool force)
    Termio *sd = evas_object_smart_data_get(obj);
    EINA_SAFETY_ON_NULL_RETURN(sd);
 
-   if (w <= 1) 
-     {
-        h = 24;
-        w = 80;
-     }
-   else if (h <= 1)
-     {
-        h = 24;
-        w = 80;
-     }
+   if (w <= 1) w = 80;
+   if (h <= 1) h = 24;
 
    if (!force)
      {
