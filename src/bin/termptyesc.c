490a065fb191e0a79ab210bf113a35ca8c8869c6
Boris Faure <billiob@gmail.com>
2017-05-17 23:34:06 +0200

    termptyesc: _csi_arg_get() could be called with nothing to read
    Could be the case when an error was already found while decoding the
    stream

--- src/bin/termptyesc.c
+++ src/bin/termptyesc.c
@@ -93,25 +93,30 @@ _csi_arg_get(Eina_Unicode **ptr)
    Eina_Unicode *b = *ptr;
    int sum = 0;
 
-   while ((*b) && (*b < '0' || *b > '9')) b++;
+   if (!b)
+     goto error;
+
+   while ((*b) && (*b < '0' || *b > '9'))
+     b++;
+
    if (!*b)
-     {
-        *ptr = NULL;
-        return 0;
-     }
+     goto error;
+
    while ((*b >= '0') && (*b <= '9'))
      {
         if (sum > INT32_MAX/10 )
-          {
-             *ptr = NULL;
-             return 0;
-          }
+          goto error;
         sum *= 10;
         sum += *b - '0';
         b++;
      }
+
    *ptr = b;
    return sum;
+
+error:
+   *ptr = NULL;
+   return -1;
 }
 
 static void
