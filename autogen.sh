c51d0095f9f107edd79c5cb63d343c347efb0471
Boris Faure <billiob@gmail.com>
2017-11-12 00:09:06 +0100

    get rid of automake

--- autogen.sh
+++ autogen.sh
@@ -1,40 +1,5 @@
 #!/bin/sh
 
-rm -rf autom4te.cache
-rm -f aclocal.m4 ltmain.sh
-
-touch README
-touch ABOUT-NLS
-
-echo "Running autopoint..." ; autopoint -f || :
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
-echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
-
-W=0
-
-rm -f config.cache-env.tmp
-echo "OLD_PARM=\"$@\"" >> config.cache-env.tmp
-echo "OLD_CFLAGS=\"$CFLAGS\"" >> config.cache-env.tmp
-echo "OLD_PATH=\"$PATH\"" >> config.cache-env.tmp
-echo "OLD_PKG_CONFIG_PATH=\"$PKG_CONFIG_PATH\"" >> config.cache-env.tmp
-echo "OLD_LDFLAGS=\"$LDFLAGS\"" >> config.cache-env.tmp
-
-cmp config.cache-env.tmp config.cache-env >> /dev/null
-if [ $? -ne 0 ]; then
-	W=1;
-fi
-
-if [ $W -ne 0 ]; then
-	echo "Cleaning configure cache...";
-	rm -f config.cache config.cache-env
-	mv config.cache-env.tmp config.cache-env
-else
-	rm -f config.cache-env.tmp
-fi
-
-if [ -z "$NOCONFIGURE" ]; then
-	./configure -C "$@"
-fi
+rm -rf build
+meson $@ . build
+mesonconf build
--- m4/efl_beta.m4
+++
@@ -1,5 +0,0 @@
-dnl use: EFL_ENABLE_BETA_API_SUPPORT
-AC_DEFUN([EFL_ENABLE_BETA_API_SUPPORT],
-[
-  AC_DEFINE([EFL_BETA_API_SUPPORT], [1], [Enable access to unstable EFL API that are still in beta])
-])
