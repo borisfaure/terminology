141fb9041b883996a1f7f113336f668ecb4353c4
Boris Faure <billiob@gmail.com>
2018-07-18 20:51:00 +0200

    pacman.sh: cleanup

--- tools/pacman.sh
+++ tools/pacman.sh
@@ -7,19 +7,17 @@
 
 initializeANSI()
 {
-    esc=$(echo -en "\033")
-  blackf="${esc}[30m";   redf="${esc}[31m";    greenf="${esc}[32m"
-  yellowf="${esc}[33m"   bluef="${esc}[34m";   purplef="${esc}[35m"
-  cyanf="${esc}[36m";    whitef="${esc}[37m"
+  esc=$(echo -en "\033")
+  redf="${esc}[31m";
+  greenf="${esc}[32m"
+  yellowf="${esc}[33m"
+  bluef="${esc}[34m";
+  purplef="${esc}[35m"
+  cyanf="${esc}[36m";
+  whitef="${esc}[37m"
 
-  blackb="${esc}[40m";   redb="${esc}[41m";    greenb="${esc}[42m"
-  yellowb="${esc}[43m"   blueb="${esc}[44m";   purpleb="${esc}[45m"
-  cyanb="${esc}[46m";    whiteb="${esc}[47m"
-
-  boldon="${esc}[1m";    boldoff="${esc}[22m"
-  italicson="${esc}[3m"; italicsoff="${esc}[23m"
-  ulon="${esc}[4m";      uloff="${esc}[24m"
-  invon="${esc}[7m";     invoff="${esc}[27m"
+  boldon="${esc}[1m";
+  boldoff="${esc}[22m"
 
   reset="${esc}[0m"
 }
