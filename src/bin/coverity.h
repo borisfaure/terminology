15ec4cb8d4f58f628e5b625b68806e355779e4c8
Boris Faure <billiob@gmail.com>
2018-11-18 17:51:34 +0100

    add some more coverity hacks

--- src/bin/coverity.h
+++ src/bin/coverity.h
@@ -4,6 +4,10 @@
 
 #ifdef __x86_64__
 typedef struct { long double x; long double y; } _Float128;
+typedef float _Float32;
+typedef float _Float32x;
+typedef double _Float64;
+typedef double _Float64x;
 #endif
 
 #endif /* __COVERITY__ */
