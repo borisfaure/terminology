88bf97f4c2259bd156435377d88b6e2c20eb4139
Boris Faure <billiob@gmail.com>
2018-11-24 15:46:35 +0100

    add tests on DECERA

---
+++ tests/decera-no-restrict-cursor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#set color
+printf '\033[46;31;3m'
+# fill space with E
+printf '\033[69;1;1;25;80$x'
+# set top/bottom margins:
+printf '\033[10;20r'
+# allow left/right margins
+printf '\033[?69h'
+# set left/right margins:
+printf '\033[5;15s'
+# top left corner
+printf '\033[3;5;12;7$z'
+# top right corner
+printf '\033[8;12;13;17$z'
+# bottom left corner
+printf '\033[18;3;22;8$z'
+# top right corner
+printf '\033[17;14;23;18$z'
