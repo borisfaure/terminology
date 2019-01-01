b76876e9c89745a9ad0c6e795a515c8f2ee28e15
Boris Faure <billiob@gmail.com>
2019-01-01 22:46:25 +0100

    tests/cursor-movements.sh: test cursor up for real

--- tests/cursor-movements.sh
+++ tests/cursor-movements.sh
@@ -11,7 +11,7 @@ printf '\033[H'
 
 # RIGHT
 # cursor right HPR / CUU
-printf 'A\033[aB\033[;aC\033[0aD'
+printf 'A\033[aB\033[;aa\033[0ab'
 printf '\033[CA\033[2CB\033[;CC'
 # test on boundaries
 printf '\033[1;80HZ\033[aA'
@@ -21,23 +21,25 @@ printf '\033[2;80HZ\033[CA'
 # go to 12;2 (CUP)
 printf '\033[2;12H'
 # cursor left (CUB)
-printf 'A\033[DB\033[;DC\033[3DD'
+printf 'C\033[DD\033[;Dc\033[3Dd'
 # Go left on start of line
-printf '\033[2;0HA\033[DB'
+printf '\033[2;0HE\033[DF'
 
 # DOWN
 # cursor down
-printf '\033[BA\033[;BB\033[3BC'
-printf '\033[eA\033[;eB\033[3eC'
+printf '\033[BG\033[;BH\033[3BI'
+printf '\033[eJ\033[;eK\033[3eL'
 # At Bottom
-printf '\033[26;0HA\033[BB'
-printf '\033[26;3HA\033[eB'
+printf '\033[26;0HM\033[BN'
+printf '\033[26;3Hm\033[en'
 
 # UP
 # cursor up
 # go to 26;6 (CUP)
 printf '\033[26;6H'
-printf 'A\033[eB\033[;eC\033[0eD'
+printf 'O\033[AP\033[;AQ\033[0AR'
+# At top
+printf '\033[0;26Hp\033[Aq'
 
 # WITH CURSOR RESTRICTION
 # set top/bottom margins:
@@ -60,7 +62,7 @@ printf '\033[6;16H#\033[C>'
 # LEFT
 printf '\033[5;0H#\033[D<'
 # UP
-printf '\033[0;5H#\033[D^'
+printf '\033[0;5H#\033[A^'
 # DOWN
 printf '\033[11;6H#\033[ev'
 printf '\033[11;8H#\033[Bv'
