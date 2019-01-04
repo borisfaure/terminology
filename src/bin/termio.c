6952836a32270828b33c07cdaf1658949f3c2693
Boris Faure <billiob@gmail.com>
2019-01-04 23:34:08 +0100

    termio: allow 1 line or 1 column windows. Closes T7532

--- src/bin/termio.c
+++ src/bin/termio.c
@@ -5404,7 +5404,7 @@ _smart_size(Evas_Object *obj, int w, int h, Eina_Bool force)
    Termio *sd = evas_object_smart_data_get(obj);
    EINA_SAFETY_ON_NULL_RETURN(sd);
 
-   if ((w <= 1) || (h <= 1))
+   if ((w <= 0) || (h <= 0))
      {
         w = 80;
         h = 24;
