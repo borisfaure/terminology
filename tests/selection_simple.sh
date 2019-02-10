aa9e74d401502bba6ba4b08507898b05670ac804
Boris Faure <billiob@gmail.com>
2019-02-10 23:54:37 +0100

    tests: fix selection_simple to force rendering

--- tests/selection_simple.sh
+++ tests/selection_simple.sh
@@ -15,15 +15,26 @@ printf '\033}tm;48;10\0'
 # mouse up
 printf '\033}tu;48;10;1;0;0\0'
 
+# force render
+printf '\033}tr\0'
+
 # selection is 'EEEEE'
 printf '\033}tiEEEEE\0'
 
 # insert E in color
 printf '\033[;4HE'
+
+# force render
+printf '\033}tr\0'
+
 # selection is 'EEEEE'
 printf '\033}tiEEEEE\0'
 
 # insert a
 printf 'a'
+
+# force render
+printf '\033}tr\0'
+
 # no more selection
-printf '\033}tn'
+printf '\033}tn\0'
