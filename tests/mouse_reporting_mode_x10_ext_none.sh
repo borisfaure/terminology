19c50c26429ad7e76019ad74530b008014619555
Boris Faure <billiob@gmail.com>
2019-03-03 18:37:12 +0100

    tests: test around mouse_x10 frontier

--- tests/mouse_reporting_mode_x10_ext_none.sh
+++ tests/mouse_reporting_mode_x10_ext_none.sh
@@ -3,6 +3,13 @@
 # char width: 7
 # char height: 15
 
+# resize window
+#resize window to 180-chars width
+printf '\033[8;;180;t'
+# force render
+printf '\033}tr\0'
+sleep 0.2
+
 # fill space with E
 printf '\033#8'
 
@@ -12,9 +19,12 @@ printf '\033[46;31;3m'
 # positions used are;
 # - 200;130 -> 9;29
 # - 480;130 -> 9;69
+# - 1230;130 -> 9;176
 # indicate the positions to help testing
 printf '\033[9;29H#'
 printf '\033[9;69H#'
+printf '\033[9;95H@' # after that one, MOUSE_X10 is not working
+printf '\033[9;176H#'
 
 # set mouse mode - X10 mode
 printf '\033[?9h'
@@ -27,9 +37,26 @@ printf '\033[1H'
 # mouse down
 printf '\033}td;200;130;1;0;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;0\0'
 # mouse up
 printf '\033}tu;480;130;1;0;0\0'
+
+# mouse click at x:93 -> ^[[M ~)
+printf '\033}td;657;130;1;0;0\0'
+printf '\033}tm;657;130;0\0'
+printf '\033}tu;657;130;1;0;0\0'
+# mouse click at x:94 -> ^[[M ^?)
+printf '\033}td;664;130;1;0;0\0'
+printf '\033}tm;664;130;0\0'
+printf '\033}tu;664;130;1;0;0\0'
+# mouse click at x:95 -> ^[[M  )
+printf '\033}td;671;130;1;0;0\0'
+printf '\033}tm;671;130;0\0'
+printf '\033}tu;671;130;1;0;0\0'
+# mouse click at x:174 -> ^[[M  )
+printf '\033}td;1230;130;1;0;0\0'
+printf '\033}tm;1230;130;0\0'
+printf '\033}tu;1230;130;1;0;0\0'
 # force render
 printf '\033}tr\0'
 sleep 0.2
@@ -41,7 +68,7 @@ printf '\033[2H'
 # mouse down
 printf '\033}td;200;130;3;0;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;0\0'
 # mouse up
 printf '\033}tu;480;130;3;0;0\0'
 # force render
@@ -55,7 +82,7 @@ printf '\033[3H'
 # mouse down
 printf '\033}td;200;130;2;0;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;0\0'
 # mouse up
 printf '\033}tu;480;130;2;0;0\0'
 # force render
@@ -86,7 +113,7 @@ printf '\033[7H'
 # mouse down
 printf '\033}td;200;130;1;1;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;1\0'
 # mouse up
 printf '\033}tu;480;130;1;1;0\0'
 # force render
@@ -101,7 +128,7 @@ printf '\033[8H'
 # mouse down
 printf '\033}td;200;130;3;1;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;1\0'
 # mouse up
 printf '\033}tu;480;130;3;1;0\0'
 # force render
@@ -116,7 +143,7 @@ printf '\033[9H'
 # mouse down
 printf '\033}td;200;130;2;1;0\0'
 # mouse move
-printf '\033}tm;480;130\0'
+printf '\033}tm;480;130;1\0'
 # mouse up
 printf '\033}tu;480;130;2;1;0\0'
 # force render
