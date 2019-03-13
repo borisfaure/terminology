2d62d58a39197392bd5e7ac762ec7b44af0514e0
Boris Faure <billiob@gmail.com>
2019-03-13 13:30:18 +0100

    tests: add dumb test on Shift In/Out

---
+++ tests/shift_in_out.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# fill space with E
+printf '\033#8'
+
+# move to 0; 0
+printf '\033[H'
+
+#set color
+printf '\033[46;31;3m'
+
+##
+# Shift In
+##
+printf '\017'
+for C in $(seq 32 64); do
+    printf "\u$C"
+done
+printf '\n'
+for C in $(seq 64 96); do
+    printf "\u$C"
+done
+printf '\n'
+for C in $(seq 96 128); do
+    printf "\u$C"
+done
+printf '\n'
+
+##
+# Shift Out
+##
+printf '\n'
+printf '\016'
+for C in $(seq 32 64); do
+    printf "\u$C"
+done
+printf '\n'
+for C in $(seq 64 96); do
+    printf "\u$C"
+done
+printf '\n'
+for C in $(seq 96 128); do
+    printf "\u$C"
+done
+printf '\n'
