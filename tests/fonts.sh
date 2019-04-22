e9cb059a32d6c0d61c0a662be3bff4e1c7725a98
Boris Faure <billiob@gmail.com>
2019-04-22 22:39:42 +0200

    update tests to reflect changes on double-width handling

--- tests/fonts.sh
+++ tests/fonts.sh
@@ -20,5 +20,5 @@ printf '\033[20mabcdefghijklmnopqrstuvwxyz\n\033[0m'
 printf '\033[20mABCDEFGHIJKLMNOPQRSTUVWXYZ\n\033[0m'
 
 printf '\nencircled\n'
-printf '\033[52ma b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4\n\033[0m'
-printf '\033[52mA B C D E F G H I J K L M N O P Q R S T U V W X Y Z 5 6 7 8 9\n\033[0m'
+printf '\033[52mabcdefghijklmnopqrstuvwxyz01234\n\033[0m'
+printf '\033[52mABCDEFGHIJKLMNOPQRSTUVWXYZ56789\n\033[0m'
