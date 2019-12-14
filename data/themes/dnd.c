bbee3978b487c0aa6137fe79603c96ffb6b78e33
Boris Faure <billiob@gmail.com>
2019-12-14 19:54:16 +0100

    DEMO show a tab (not correct for the moment)

--- data/themes/dnd.c
+++ data/themes/dnd.c
@@ -9,6 +9,29 @@
 #define WIDTH  400
 #define HEIGHT 400
 
+typedef struct _Tab_Item Tab_Item;
+struct _Tab_Item {
+     const char *title;
+     Eina_Bool has_bell;
+};
+
+static const char  *_edje_file = "dnd.edj";
+static Ecore_Evas  *_ee = NULL;
+static Evas        *_evas = NULL;
+static Evas_Object *_bg = NULL;
+static Evas_Object *_tab_bar= NULL;
+static Evas_Object *_main_tab = NULL;
+static Evas_Object *_spacer = NULL;
+
+static Tab_Item _tab_items[4] = {
+       {.title="tab 1", .has_bell=EINA_FALSE,},
+       {.title="tab 2", .has_bell=EINA_FALSE,},
+       {.title="tab 3", .has_bell=EINA_FALSE,},
+       {.title="tab 4", .has_bell=EINA_FALSE,},
+};
+
+
+
 static void
 _on_destroy(Ecore_Evas *ee EINA_UNUSED)
 {
@@ -20,63 +43,86 @@ _on_destroy(Ecore_Evas *ee EINA_UNUSED)
 static void
 _on_canvas_resize(Ecore_Evas *ee)
 {
-   Evas_Object *bg;
-   Evas_Object *edje_obj;
    int          w;
    int          h;
 
-   bg = ecore_evas_data_get(ee, "background");
-   edje_obj = ecore_evas_data_get(ee, "edje_obj");
-
    ecore_evas_geometry_get(ee, NULL, NULL, &w, &h);
-   evas_object_resize(bg, w, h);
-   evas_object_resize(edje_obj, w, h);
+   evas_object_resize(_bg, w, h);
 }
 
+static void
+_tabbar_setup(void)
+{
+   Evas_Coord w = 0, h = 0;
+
+   _tab_bar = edje_object_add(_evas);
+   if (!edje_object_file_set(_tab_bar, _edje_file, "tab_bar"))
+     printf("failed to set file %s.\n", _edje_file);
+
+
+   _spacer = evas_object_rectangle_add(_evas);
+   evas_object_color_set(_spacer, 0, 0, 0, 0);
+   //elm_coords_finger_size_adjust(1, &w, 1, &h);
+   w = h = 32;
+   evas_object_size_hint_min_set(_spacer, w, h);
+   edje_object_part_swallow(_tab_bar, "terminology.tab_spacer",
+                            _spacer);
+
+   _main_tab = edje_object_add(_evas);
+   if (!edje_object_file_set(_main_tab, _edje_file, "main_tab"))
+     printf("failed to set file %s.\n", _edje_file);
+   edje_object_part_swallow(_tab_bar, "terminology.main_tab",
+                            _main_tab);
+   evas_object_show(_main_tab);
+   edje_object_part_text_set(_main_tab, "terminology.tab.title",
+                             "foo bar 42");
+
+
+   edje_object_part_swallow(_bg, "terminology.tab_bar",
+                            _tab_bar);
+   evas_object_show(_tab_bar);
+   edje_object_signal_emit(_bg, "tab_bar,on", "terminology");
+   _main_tab = edje_object_add(_evas);
+}
 
 
 int
 main(int argc EINA_UNUSED, char *argv[] EINA_UNUSED)
 {
-   const char  *edje_file = "dnd.edj";
-   Ecore_Evas  *ee;
-   Evas        *evas;
-   Evas_Object *bg;
-   Evas_Object *edje_obj;
 
    assert(ecore_evas_init());
    assert(edje_init());
-   ee = ecore_evas_new(NULL, 0, 0, WIDTH, HEIGHT, NULL);
-   assert(ee);
+   _ee = ecore_evas_new(NULL, 0, 0, WIDTH, HEIGHT, NULL);
+   assert(_ee);
 
-   ecore_evas_callback_destroy_set(ee, _on_destroy);
-   ecore_evas_callback_resize_set(ee, _on_canvas_resize);
-   ecore_evas_title_set(ee, "Tab Drag Test");
+   ecore_evas_callback_destroy_set(_ee, _on_destroy);
+   ecore_evas_callback_resize_set(_ee, _on_canvas_resize);
+   ecore_evas_title_set(_ee, "Tab Drag Test");
 
-   evas = ecore_evas_get(ee);
+   _evas = ecore_evas_get(_ee);
 
-   edje_obj = edje_object_add(evas);
+   _bg = edje_object_add(_evas);
 
-   if (!edje_object_file_set(edje_obj, edje_file, "main"))
-     printf("failed to set file %s.\n", edje_file);
+   if (!edje_object_file_set(_bg, _edje_file, "main"))
+     printf("failed to set file %s.\n", _edje_file);
 
-   evas_object_move(edje_obj, 0, 0);
-   evas_object_resize(edje_obj, WIDTH, HEIGHT);
-   evas_object_show(edje_obj);
-   ecore_evas_data_set(ee, "edje_obj", edje_obj);
+   evas_object_move(_bg, 0, 0);
+   evas_object_resize(_bg, WIDTH, HEIGHT);
+   evas_object_show(_bg);
+   ecore_evas_data_set(_ee, "bg", _bg);
 
-   ecore_evas_show(ee);
+
+   _tabbar_setup();
+
+   /* TODO */
+
+   ecore_evas_show(_ee);
 
    ecore_main_loop_begin();
 
-   ecore_evas_free(ee);
+   ecore_evas_free(_ee);
    ecore_evas_shutdown();
    edje_shutdown();
 
    return EXIT_SUCCESS;
-
- shutdown_ecore_evas:
-   ecore_evas_shutdown();
-
-   return EXIT_FAILURE;
 }
