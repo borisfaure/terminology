7ebc43f6115a8112864c6471ec691089efb347be
Boris Faure <billiob@gmail.com>
2019-12-15 22:44:26 +0100

    DEMO main tab can be set as draggable

--- data/themes/dnd.c
+++ data/themes/dnd.c
@@ -29,14 +29,18 @@ static Evas_Object *_win = NULL;
 static Evas_Object *_bg = NULL;
 static Evas_Object *_tab_bar= NULL;
 static Evas_Object *_main_tab = NULL;
+static Evas_Object *_left_box = NULL;
+static Evas_Object *_right_box = NULL;
+static Evas_Object *_spacer = NULL;
 
-static int _tab_active_idx = 0;
+static int _tab_active_idx = 2;
 
-static Tab_Item _tab_items[4] = {
-       {.title="tab 1", .has_bell=EINA_FALSE,},
-       {.title="tab 2", .has_bell=EINA_FALSE,},
-       {.title="tab 3", .has_bell=EINA_FALSE,},
-       {.title="tab 4", .has_bell=EINA_FALSE,},
+#define NB_TABS  4
+static Tab_Item _tab_items[NB_TABS] = {
+       {.title="tab 1 alpha bravo charlie delta", .has_bell=EINA_FALSE,},
+       {.title="tab 2 echo foxtrot golf hotel", .has_bell=EINA_FALSE,},
+       {.title="tab 3 india juliett kilo lima", .has_bell=EINA_FALSE,},
+       {.title="tab 4 mike november osca papa", .has_bell=EINA_FALSE,},
 };
 
 
@@ -86,7 +90,56 @@ _cb_main_tab_change(void *data,
 
    evas_object_geometry_get(obj, NULL, NULL, &w, &h);
    ERR("main tab w:%d h:%d", w, h);
-   //evas_object_size_hint_min_set(_main_tab, w, h);
+   evas_object_size_hint_min_set(_spacer, 1, h);
+}
+
+static void
+_tab_bar_clear(void)
+{
+   elm_box_clear(_left_box);
+   elm_box_clear(_right_box);
+}
+
+static void
+_tab_bar_fill(void)
+{
+   int i;
+   _tab_bar_clear();
+
+   for (i = 0; i < NB_TABS; i++)
+     {
+        Tab_Item *item = _tab_items + i;
+
+        if (i == _tab_active_idx)
+          {
+             double v1, v2;
+
+             edje_object_part_text_set(_main_tab, "terminology.tab.title",
+                                       item->title);
+
+
+             v1 = (double)(i) / (double)NB_TABS;
+             v2 = (double)(i+1) / (double)NB_TABS;
+
+             edje_object_part_drag_value_set(_bg, "terminology.main_tab",
+                                             v1, 0.0);
+             edje_object_part_drag_size_set(_bg, "terminology.main_tab",
+                                             v2 - v1, 0.0);
+             continue;
+          }
+     }
+}
+
+static void
+_on_drag(void *data EINA_UNUSED,
+         Evas_Object *o,
+         const char *emission EINA_UNUSED,
+         const char *source EINA_UNUSED)
+{
+   double val;
+
+   edje_object_part_drag_value_get(o, "terminology.main_tab", NULL, &val);
+   ERR("value changed to: %0.3f", val);
 }
 
 static void
@@ -94,23 +147,51 @@ _tab_bar_setup(void)
 {
    Evas_Coord w = 0, h = 0;
 
+   _spacer = evas_object_rectangle_add(_evas);
+   evas_object_color_set(_spacer, 0, 0, 0, 0);
+   elm_coords_finger_size_adjust(1, &w, 1, &h);
+   evas_object_size_hint_min_set(_spacer, w, h);
+   edje_object_part_swallow(_bg, "terminology.tab",
+                            _spacer);
+   evas_object_show(_spacer);
+
    _main_tab = edje_object_add(_evas);
    if (!edje_object_file_set(_main_tab, _edje_file, "tab_main"))
      printf("failed to set file %s.\n", _edje_file);
-   edje_object_part_swallow(_bg, "terminology.tab_main",
+   edje_object_part_swallow(_bg, "terminology.main_tab",
                             _main_tab);
    edje_object_part_text_set(_main_tab, "terminology.tab.title",
                              "foo bar 42 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
    evas_object_size_hint_weight_set(_main_tab, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_fill_set(_main_tab, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   edje_object_signal_callback_add(_main_tab, "drag", "terminology.main_tab",
+                                   _on_drag, NULL);
 
    edje_object_size_min_calc(_main_tab, &w, &h);
+   ERR("min: %d %d", w, h);
    evas_object_size_hint_min_set(_main_tab, w, h);
    evas_object_event_callback_add(_main_tab, EVAS_CALLBACK_MOVE,
                                   _cb_main_tab_change, NULL);
    evas_object_event_callback_add(_main_tab, EVAS_CALLBACK_RESIZE,
                                   _cb_main_tab_change, NULL);
    evas_object_show(_main_tab);
+
+   _left_box = elm_box_add(_win);
+   assert(_left_box);
+   elm_box_horizontal_set(_left_box, EINA_TRUE);
+   elm_box_homogeneous_set(_left_box, EINA_FALSE);
+   evas_object_size_hint_weight_set(_left_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_fill_set(_left_box, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   edje_object_part_swallow(_bg, "terminology.tabs_left", _left_box);
+
+   _right_box = elm_box_add(_win);
+   assert(_right_box);
+   elm_box_horizontal_set(_right_box, EINA_TRUE);
+   elm_box_homogeneous_set(_right_box, EINA_FALSE);
+   evas_object_size_hint_weight_set(_right_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_fill_set(_right_box, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   edje_object_part_swallow(_bg, "terminology.tabs_right", _right_box);
+
 
    edje_object_signal_emit(_bg, "tab_bar,on", "terminology");
    evas_object_show(_tab_bar);
@@ -158,6 +239,7 @@ elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 
 
    _tab_bar_setup();
+   _tab_bar_fill();
 
    /* TODO */
 
