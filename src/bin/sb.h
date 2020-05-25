0051a28996161834e87fce34165f099492c3966a
Boris Faure <billiob@gmail.com>
2020-05-25 22:38:39 +0200

    termiolink: add tests on some parsing functions

--- src/bin/sb.h
+++ src/bin/sb.h
@@ -11,9 +11,11 @@ struct ty_sb {
 };
 
 int ty_sb_add(struct ty_sb *sb, const char *s, size_t len);
+#define TY_SB_ADD(_SB, _S) ty_sb_add(_SB, _S, strlen(_S))
 void ty_sb_spaces_rtrim(struct ty_sb *sb);
 void ty_sb_spaces_ltrim(struct ty_sb *sb);
 int ty_sb_prepend(struct ty_sb *sb, const char *s, size_t len);
+#define TY_SB_PREPEND(_SB, _S) ty_sb_prepend(_SB, _S, strlen(_S))
 char *ty_sb_steal_buf(struct ty_sb *sb);
 void ty_sb_lskip(struct ty_sb *sb, size_t len);
 void ty_sb_rskip(struct ty_sb *sb, size_t len);
--- src/bin/unit_tests.h
+++ src/bin/unit_tests.h
@@ -10,5 +10,9 @@ int tytest_sb_skip(void);
 int tytest_sb_trim(void);
 int tytest_sb_gap(void);
 int tytest_sb_steal(void);
+int tytest_color_parse_hex(void);
+int tytest_color_parse_2hex(void);
+int tytest_color_parse_sharp(void);
+int tytest_color_parse_uint8(void);
 
 #endif
