cc552d7ea5546759d4b5446c9fd13e9661c1ff29
Boris Faure <billiob@gmail.com>
2020-09-01 19:08:52 +0200

    win: make code more "as usual"

--- src/bin/win.c
+++ src/bin/win.c
@@ -7374,7 +7374,7 @@ term_new(Win *wn, Config *config, const char *cmd,
    evas_object_show(o);
 
    term->bg = o = elm_layout_add(wn->win);
-   term->bg_edj = elm_layout_edje_get(term->bg);
+   term->bg_edj = elm_layout_edje_get(o);
    evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_fill_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
    if (!theme_apply_elm(o, config, "terminology/background"))
