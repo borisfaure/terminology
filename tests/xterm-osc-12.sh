910a78c9bcbcfb7a2b9431ba0f6bb9de9982b1f8
Boris Faure <billiob@gmail.com>
2020-11-15 20:17:53 +0100

    tests: add test on escape code OSC 12

---
+++ tests/xterm-osc-12.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# char width: 7
+# char height: 15
+
+# set color
+printf '\033[0;31;3m'
+
+# clear screen
+printf '\033[2J'
+
+# move to 0; 0
+printf '\033[0;0H'
+
+printf 'The purpose of computing is insight, not numbers.\r\n'
+printf 'Richard Hamming\r\n'
+
+# invalid colors
+printf '\033]12;#ff0\007'
+printf '\033]12;#FfFfFfFf\007'
+printf '\033]12;fff000\007'
+# change cursor color and then query it
+printf '\033]12;#ff00FF\007'
+printf '\033]12;?\007'
+sleep 0.1
