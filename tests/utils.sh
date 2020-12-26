ba9d951b28393187dbd39c3b7a65c3a915ed9389
Boris Faure <billiob@gmail.com>
2020-12-26 18:35:40 +0100

    sh: compare strings but not illegal numbers
    occurs when IN_TY_TEST_UI is not defined

--- tests/utils.sh
+++ tests/utils.sh
@@ -3,7 +3,7 @@
 test_sleep()
 {
     #only sleep when running the test within terminology with test ui on
-    if [ "$IN_TY_TEST_UI" -eq "1" ]; then
+    if [ "$IN_TY_TEST_UI" = "1" ]; then
         sleep "$1"
     fi
 }
